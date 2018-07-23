0110da8e413794e12dcb6ac5ced297ad2e3a052c
azharprabudi <azharprabui@gmail.com>
2018-07-23 19:55:59 +0700

    add new task

---
+++ task/array/task-array-p3/index.js
@@ -0,0 +1,17 @@
+/*
+
+Terdapat sebuah fungsi yang menerima 1 parameter dengan tipe data Array, tugas anda adalah diminta untuk mengembalikan nilai besar dan nilai kecil. Yang dimana nilai terkecil dalam sebuah array berada pada index ke 0, dan nilai terbesar dalam sebuah array ada di index ke 1
+
+contoh :
+
+getMinMax([5,3,2,0,-1,11]) akan mengembalikan [-1, 11];
+
+untuk mengecek apakah logika dari fungsi yang dibuat benar atau tidak jalankan ini pada terminal anda
+
+npm run jest ./task/array/task-array-p3/test.js
+
+*/
+
+const getMinMax = arr => {};
+
+module.exports = getMinMax;
