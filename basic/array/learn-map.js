0dadd1580d6684d4504d0c71ffec6ccf18001afc
azharprabudi <azharprabui@gmail.com>
2018-07-23 20:10:26 +0700

    learn map

---
+++ basic/array/learn-map.js
@@ -0,0 +1,35 @@
+/*
+
+Map merupakan fungsi dari javascript yang bertujuan untuk melakukan pengulangan dari sebuah array tapi akan mengembalikan array baru, dan dapat kita modifikasi ketika proses looping terjadi
+
+Contoh 1 :
+
+var arrayNumber = [5, 4, 3, 2];
+arrayNumber = arrayNumber.map(item => item * 2); // maka akan menghasilkan [10, 8, 6, 4]
+
+Contoh 2 :
+
+var arrayObject = [{ name: 'azhar', age: 21 }, { name: 'siti', age: 21 }];
+arrayObject = arrayObject.map(item => Object.assign({}, item, {age: item.age + 2})); // maka akan menghasilkan [{name: 'azhar', age: 23}, {name: 'siti', age: 23}];
+
+*/
+
+const learnMap = () => {
+  var arrayObject = [
+    { name: "azhar", age: 21, city: "jakarta" },
+    { name: "siti", age: 21, city: "jakarta" },
+    { name: "rian", age: 21, city: "jakarta" },
+    { name: "zahra", age: 21, city: "jakarta" }
+  ];
+
+  arrayObject = arrayObject.map(item =>
+    Object.assign({}, item, { age: item.age + 2 })
+  );
+
+  console.log(arrayObject);
+  return arrayObject;
+};
+
+learnMap();
+
+module.exports = learnMap;
