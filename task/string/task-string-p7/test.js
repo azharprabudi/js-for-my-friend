c7f7939bea7ec85ea5f82a8f9cb0b88912d63633
sitizuliatul <sitizuliatul@gmail.com>
2018-08-12 10:41:38 +0700

    tast 8 done

--- task/string/task-string-p7/test.js
+++ task/string/task-string-p7/test.js
@@ -1,5 +1,5 @@
 const jajarGenjang = require("./index");
 
 test("should return true", () => {
-  expect(jajarGenjang(3, 5, 3)).toEqual("  ***\n  ***\n ***\n");
+  expect(jajarGenjang(3, 5, 3)).toEqual("  ***\n ***\n***\n");
 });
--- task/string/task-string-p8/index.js
+++ task/string/task-string-p8/index.js
@@ -16,6 +16,24 @@ output :
 
 */
 
-const makeRectangle = numb => {};
+const makeRectangle = numb => {
+
+    let tmp ="";
+
+    for (let i=1 ;i<=numb ; i++){
+        for(let j=1 ;j<=numb ;j++){
+            if( i == 1 || i==numb || j==1 || j==numb ){
+                tmp += '*';
+            } else {
+                tmp += ' ';
+            }
+        }
+        tmp += "\n";
+    }
+
+    console.log(tmp);
+    return tmp;
+};
+
 
 module.exports = makeRectangle;
