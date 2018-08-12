c607f76e77dae2cf6bb8760a084c4b525f37d7b1
sitizuliatul <sitizuliatul@gmail.com>
2018-08-12 12:03:58 +0700

    tast 9 done

--- task/string/task-string-p9/index.js
+++ task/string/task-string-p9/index.js
@@ -16,6 +16,23 @@ output :
 
 */
 
-const makeRectangle = numb => {};
+const makeRectangle = numb => {
+
+    let tmp ="";
+    let k =  Math.ceil(numb / 2)
+
+    for (let i = 1 ; i<=numb ;i++){
+        for (let j = 1 ; j<=numb ;j++){
+            if (i==1 || i==numb || j == k || j==1  || j==numb  ){
+                tmp += '*';
+            }else{
+                tmp += ' ';
+            }
+        }
+        tmp += "\n";
+    }
+    console.log(tmp);
+    return tmp;
+};
 
 module.exports = makeRectangle;
--- task/string/task-string-p9/test.js
+++ task/string/task-string-p9/test.js
@@ -2,6 +2,6 @@ const makeRectangle = require("./index");
 
 test("i will pass number 7 to the function", () => {
   expect(makeRectangle(7)).toEqual(
-    "*******\n*  *  *\n*  *  *\n*  *  *\n*  *  *\n*******"
+    "*******\n*  *  *\n*  *  *\n*  *  *\n*  *  *\n*  *  *\n*******\n"
   );
 });
